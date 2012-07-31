149b697985bff4076dac8f40b58125a6bbc28307
Pierce Moore <Pierce@PierceMoore.com>
2012-07-31 13:12:33 -0500

    Integrated Twitter Bootstrap

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+	<head>
+		<title>chillgr.im | the best in chillout</title>
+
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+	</head>
+
+	<body>
+
+		<h1>This is the chillgr.im home page</h1>
+
+	</body>
+</html>	
