9e72e187c1fe6b28532338f7817500b36a5dac33
Pierce Moore <piercemoore@gmail.com>
2012-08-01 02:15:55 -0500

    Built page entirely, added analytics tracking
    Embedded chillgr.im and badass chillout playlists

--- index.php
+++ index.php
@@ -1,22 +1,71 @@
 <!DOCTYPE html>
+<html lang="en">
 	<head>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-7133211-17']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
 		<title>chillgr.im | the best in chillout</title>
 
 		<link rel="shortcut icon" href="img/favicon.ico" />
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
-		<link rel="stylesheet" type="text/css" href="css/style.css" />
 
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
 
 	</head>
 
-	<body>
-		<div class="container">
-			<div class="span6 offset3 main">
+	<body class="container">
+		<header class="row" id="chill_header">
+			<div class="span6 offset3">
 				<p class="logo">chillgr.im</p>
+				<br />
 				<p>Bringing you the best chillout, downtempo, and trip hop you've ever heard.</p>
 			</div>
+		</header>
+
+		<nav class="row" id="chill_nav">
+			<div class="span4 offset2">
+				<p class="subheader">chillgr.im mix</p>
+			</div>
+
+			<div class="span4">
+				<p class="subheader">massive chillout</p>
+			</div>
+		</nav>
+
+		<div class="row">
+			<div class="span4 offset2">
+				<iframe src="https://embed.spotify.com/?uri=spotify:user:piercemoore:playlist:0gixIhLYC54z2V0mhWIjyC" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>
+			</div>
+			<div class="span4">
+				<iframe src="https://embed.spotify.com/?uri=spotify:user:piercemoore:playlist:7EOiUXfkXj8GmC8LAQmPRM" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>
+			</div>
 		</div>
+
+		<br />
+
+		<div class="row">
+			<div class="span4 offset2">
+				<a class="btn btn-large btn-primary" id="btn_playlists" href="http://open.spotify.com/user/piercemoore/playlist/0gixIhLYC54z2V0mhWIjyC">Open in Spotify</a>
+			</div>
+
+			<div class="span4">
+				<a class="btn btn-large btn-primary" id="btn_playlists" href="http://open.spotify.com/user/piercemoore/playlist/7EOiUXfkXj8GmC8LAQmPRM">Open in Spotify</a>
+			</div>
+		</div>
+
 	</body>
 </html>	
